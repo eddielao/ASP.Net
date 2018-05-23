44d14ecb784347fb5d230984b3da416cc60d4c2f
Eddie Lao <eddielao@gmail.com>
2018-05-22 19:29:39 -0700

    Created database Calculator with table dbo.tblCalculator.

---
+++ lab1/lab1/App_Data/dbo.tblCalculator.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblCalculator]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [Operand1] DECIMAL(18, 2) NOT NULL, 
+    [Operator] CHAR NOT NULL, 
+    [Operand2] DECIMAL(18, 2) NOT NULL, 
+    [Result] DECIMAL(38, 2) NOT NULL, 
+    [TimeStamp] DATETIME NOT NULL, 
+    [UserName] VARCHAR(50) NULL
+)
